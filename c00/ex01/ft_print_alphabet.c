f4972903db663ef9af353adc168866a5be7f9684
beyzaslanhan <79101761+Beyzaslnhn@users.noreply.github.com>
2022-02-18 13:36:26 +0300

    Add files via upload

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eayasli <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/27 13:23:23 by eayasli           #+#    #+#             */
+/*   Updated: 2021/11/27 13:27:23 by eayasli          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		ft_putchar(alphabet);
+		alphabet = alphabet + 1;
+	}
+}	
