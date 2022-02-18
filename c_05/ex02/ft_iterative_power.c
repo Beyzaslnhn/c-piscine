3ff4ad2f464b48097e6c4dbfb1c0d82f827a1531
beyzaslanhan <79101761+Beyzaslnhn@users.noreply.github.com>
2022-02-18 13:40:33 +0300

    %77

---
+++ c_05/ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baslanha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/11 20:43:12 by baslanha          #+#    #+#             */
+/*   Updated: 2021/12/11 20:43:15 by baslanha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	result = nb;
+	while (power > 1)
+	{
+		result *= nb;
+		power--;
+	}
+	if (power == 0)
+		return (1);
+	if (power < 0)
+		return (0);
+	return (result);
+}
---
+++ c_05/ex03/ft_recursive_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baslanha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/11 20:44:42 by baslanha          #+#    #+#             */
+/*   Updated: 2021/12/11 20:45:22 by baslanha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	n;
+
+	n = nb;
+	if (n > n * nb)
+		return (0);
+	else if (power > 1)
+		return (n * ft_recursive_power(n, power - 1));
+	else if (power == 0)
+		return (1);
+	else if (power < 0)
+		return (0);
+	else
+		return (n);
+}
