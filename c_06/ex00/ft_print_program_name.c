6415f5c530e2f24203490ab592a190a77c40dfbe
beyzaslanhan <79101761+Beyzaslnhn@users.noreply.github.com>
2022-02-18 13:40:54 +0300

    %100

---
+++ c_06/ex00/ft_print_program_name.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baslanha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 10:58:39 by baslanha          #+#    #+#             */
+/*   Updated: 2021/12/14 14:25:19 by baslanha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 0;
+	while (argv[0][i] != '\0' && argc)
+	{
+		ft_putchar(argv[0][i]);
+		i++;
+	}
+	ft_putchar('\n');
+	return (0);
+}
---
+++ c_06/ex01/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baslanha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 11:11:13 by baslanha          #+#    #+#             */
+/*   Updated: 2021/12/14 15:29:19 by baslanha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j < argc)
+	{
+		i = 0;
+		while (argv[j][i] != '\0')
+		{
+			ft_putchar(argv[j][i]);
+			i++;
+		}
+		ft_putchar('\n');
+		j++;
+	}
+	return (0);
+}
